354e44bed33591e14220f94b86560952099e8428
nsx_69 <mrmanafest16@gmail.com>
2024-01-07 18:19:01 +0500

    tasks code mu

---
+++ script.js
@@ -0,0 +1,49 @@
+// let a = '5' + '2';
+// alert(a);
+
+// let a = '5' + 2;
+// alert(a);
+
+// let a = 5 + '2';
+// alert(a);
+
+// let a = 5 + 2;
+// alert(a);
+
+// let a = '5' * '2';
+// alert(a);
+
+// let a = '5' - '2';
+// alert(a);
+
+// let a = '5' % '2';
+// alert(a);
+
+// let a = '5s' + '2';
+// alert(a);
+
+// let a = '5' * '1' + '2' * '1';
+// alert(a);
+
+// let a = '' + 3 + 1;
+// alert(a);
+
+// let a = '2';
+// let b = '3';
+// alert(a + b);
+
+// let a = '2';
+// let b = '3';
+// alert(Number(a) + Number(b));
+
+// alert( Number('2') + Number('3') );
+
+// alert( 2 + Number('3') );
+
+// alert( '2' + Number('3') );
+
+// let a = +'2';
+// let b = +'3';
+// alert(a + b);
+
+// alert(Number('2s'));
